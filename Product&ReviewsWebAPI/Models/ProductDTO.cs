c5bcb8a4decfadc801833297c55df45509605b74
Andrea Calderon <andreacalderon@MacBook-Pro-2.local>
2023-11-21 12:37:10 -0500

    added starter files and created Product and Review Classes

---
+++ Product&ReviewsWebAPI/Models/ProductDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Product_ReviewsWebAPI.Models
+{
+	public class ProductDTO
+	{
+		public string Name { get; set; }
+		public double Price { get; set; }
+		public ICollection<Review> Reviews { get; set; }
+		public double AverageRating { get; set; }
+	}
+}
+
---
+++ Product&ReviewsWebAPI/Models/ReviewDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Product_ReviewsWebAPI.Models
+{
+	public class ReviewDTO
+	{
+		public string Text { get; set; }
+		public int Rating { get; set; }
+	}
+}
+
